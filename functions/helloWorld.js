b45578465b17e91314915f291e7b568fe2d260b5
Brett Hurst <bretthurst@outlook.com>
2019-07-18 00:57:11 -0700

    CL

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,4 +1,6 @@
 exports.handler = function (event, context, callback) {
+    console.log(event)
+    console.log(context)
     callback(null, {
         statusCode: 200,
         body: `<html><body><h1>Hello World AGAIN!! AGAIN</h1></body></html>`
